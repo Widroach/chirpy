e2d6a378065cdb78174398294a7b0d75c5c9b834
Zalán Hegedüs <zalan.hegedus@gmail.com>
2026-07-19 21:51:41 +0200

    CH1-L8: Add /healthz route

--- main.go
+++ main.go
@@ -11,7 +11,14 @@ func main() {
 		Handler: serveMux,
 		Addr:    ":8080",
 	}
-	serveMux.Handle("/",http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	serveMux.HandleFunc("/healthz", healthzController)
 
 	log.Fatal(server.ListenAndServe())
+}
+
+func healthzController(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
