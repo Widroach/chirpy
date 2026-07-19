661cfec49183f97772486ea8a65d1b6919dcb379
Zalán Hegedüs <zalan.hegedus@gmail.com>
2026-07-19 22:42:11 +0200

    CH2-L3: Add HTTP method constraints

--- main.go
+++ main.go
@@ -18,9 +18,9 @@ func main() {
 		Addr:    ":8080",
 	}
 	serveMux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
-	serveMux.HandleFunc("/healthz", healthzController)
-	serveMux.HandleFunc("/metrics", cfg.hitsController)
-	serveMux.HandleFunc("/reset", cfg.resetHitsController)
+	serveMux.HandleFunc("GET /healthz", healthzController)
+	serveMux.HandleFunc("GET /metrics", cfg.hitsController)
+	serveMux.HandleFunc("POST /reset", cfg.resetHitsController)
 
 	log.Fatal(server.ListenAndServe())
 }
