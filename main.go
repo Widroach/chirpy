336cd0b089a96ea5c33723537c265dbb49e8023a
Zalán Hegedüs <zalan.hegedus@gmail.com>
2026-07-19 22:39:57 +0200

    CH2-L2: Add metrics and reset endpoint

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
+)
+
+var (
+	cfg = apiConfig{}
 )
 
 func main() {
@@ -11,8 +17,10 @@ func main() {
 		Handler: serveMux,
 		Addr:    ":8080",
 	}
-	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	serveMux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	serveMux.HandleFunc("/healthz", healthzController)
+	serveMux.HandleFunc("/metrics", cfg.hitsController)
+	serveMux.HandleFunc("/reset", cfg.resetHitsController)
 
 	log.Fatal(server.ListenAndServe())
 }
@@ -21,4 +29,29 @@ func healthzController(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) hitsController(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	text := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	w.Write([]byte(text))
+}
+
+func (cfg *apiConfig) resetHitsController(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Success"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
 }
